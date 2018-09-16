6116c7695fdbe5ea2aeb899fcf3c68159fff4746
walter <walter.fengnz@gmail.com>
2018-09-16 21:22:02 +1200

    add image recognition

--- frontend/src/components/ImageLinkForm/ImageLinkForm.js
+++ frontend/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 	return (
 		<div>
 			<p className='f3'>
 				{'Taorui will detect faces in your photo:-)'}
 			</p>
 			<div className='center'>
-				<div className='form center pa4 br3 shadow-5'>
+				<div className='form center pa4 br3 shadow-5' onChange={onInputChange}>
 					<input className='f4 pa2 w-70 center' type='text' />
-					<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+					<button 
+						className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' 
+						onClick={onButtonSubmit}
+					>
+						Detect
+					</button>
 				</div>
 			</div>
 		</div>
