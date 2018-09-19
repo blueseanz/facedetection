42450283f6465c420dbd8c111558cecdae23e44a
Ming <ming.hui.fengnz@gmail.com>
2018-09-19 22:00:41 +1200

    face dectection box

--- frontend/src/components/FaceRecognition/FaceRecognition.js
+++ frontend/src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
 	return (
 		<div className='center ma'>
 			<div className='absolute mt2'>
-				<img alt={'face detection'} src={imageUrl} width='500px' height='auto'/>
+				<img id='inputimage' alt={'face detection'} src={imageUrl} width='500px' height='auto'/>
+				<div className='bounding-box' style={{top: box.rightCol, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
 			</div>
 		</div>
 	);
