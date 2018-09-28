85ba02735afd8ac3ae61b8f710123d0ae1392a37
walter <walter.fengnz@gmail.com>
2018-09-28 18:50:58 +1200

    connecting to postgresql database and registering a user

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,21 @@ const app = express();
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'admin',
+		database: 'smart-brain'
+	}
+})
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+})
 
 const database = {
 	users: [
@@ -47,14 +62,17 @@ app.post('/signin', (req,res) => {
 
 app.post('/register', (req, res) => {
 	const {email, name, password } = req.body;
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		entries: 0,
-		joined: new Date()
-	})
-	res.json(database.users[database.users.length-1]);
+	db('users')
+		.returning('*')
+		.insert({
+			email: email,
+			name: name,
+			joined: new Date()
+		})
+		.then(user => {
+			res.json(user[0]);
+		})
+		.catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
